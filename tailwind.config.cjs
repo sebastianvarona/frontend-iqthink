36fd962e566d50460ee551918d2147ba50ec16ff
Sebastian Varona <sebastianvarona@gmail.com>
2022-08-04 13:29:19 -0500

    added tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
